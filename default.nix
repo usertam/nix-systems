75cce051ab62d68d0ab9d9a74bbbe283a7e22bbd
usertam <heiyiutam@gmail.com>
2024-12-28 19:05:30 +0800

    linux-only systems

--- default.nix
+++ default.nix
@@ -1,7 +1,5 @@
 [
-  "aarch64-darwin"
   "aarch64-linux"
-  "x86_64-darwin"
   "x86_64-linux"
   "riscv64-linux"
 ]
--- flake.nix
+++ flake.nix
@@ -1,9 +1,7 @@
 {
   outputs = _: {
     systems = [
-      "aarch64-darwin"
       "aarch64-linux"
-      "x86_64-darwin"
       "x86_64-linux"
       "riscv64-linux"
     ];
